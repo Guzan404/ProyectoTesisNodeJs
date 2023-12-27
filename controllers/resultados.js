fc8409c6151ec24cd7a14741d02bf5ef7cfacd25
Guzan404 <diego.gonzalezz1997@gmail.com>
2023-12-26 22:18:38 -0300

    finalizado proyecto

--- controllers/resultados.js
+++ controllers/resultados.js
@@ -5,12 +5,11 @@ exports.mostrarResultados = async (req, res, next) => {
     const userId = req.user._id;
 
     // Busca todos los resultados asociados al usuario actual
-    const resultados = await Test.find({ userId }).populate('estudianteId');
+    const resultados = await Test.find({ userId }).populate('estudianteId textoId');
     
     if (!resultados || resultados.length === 0) {
       return res.status(404).render('error', { error: 'Resultados no encontrados' });
     }
-
     res.render('resultados/resultadosView', {
       pageTitle: 'Resultados de Prueba',
       path: 'resultados/resultadosView',
